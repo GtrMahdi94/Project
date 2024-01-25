15afdf9b29e252cd4ef44d0a09b78a7b87ad0930
Gatri Mahdi <gatrimahdi5@gmail.com>
2024-01-25 22:48:11 +0100

    almost

--- sudoku.js
+++ sudoku.js
@@ -1,61 +1,99 @@
-// test linked :
+var score = 0;
+var mistakes = 0;
 
-// principal Matrix :
-//var Matrix=[[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null]]
-// input variable :
-var x =$("sudoku")
-clicked()
-
-
-//function makeSudoku cases :
-var arr = [];
- 
-    function makeSudoku() {
-        for (var i = 1; i <= 9; i++) {
-            for (var j = 1; j <= 9; j++) {
-                var cas = document.createElement("input");
-                cas.id =   i + " " + j;
-                cas.classList.add("cube");
-                $(".sudoku").append(cas);
-                arr.push(cas.id);
-            }
+function makeSudoku() {
+    for (var i = 1; i <= 9; i++) {
+        for (var j = 1; j <= 9; j++) {
+            var cas = document.createElement("input");
+            cas.classList.add("cube");
+            cas.setAttribute("type", "text");
+            $(".sudoku").append(cas);
         }
     }
-
+}
 
 function clicked() {
-    $(".cube").on("click", function () {
-        var clickedId = this.id;
-        console.log("Clicked ID: " + clickedId);
+    $(".cube").on("input", function () {
+        var enteredValue = $(this).val();
+        console.log("Typed Value: " + enteredValue);
+
+        if (enteredValue) {
+            var isMistake = checkMistakes(this);
+            if (!isMistake) {
+                updateScore();
+            } else {
+                // Clear the input value for mistakes
+                $(this).val("");
+            }
+            updateMistakes();
+        }
     });
-    
-}
-   
-
-
-
-            
-makeSudoku()
-// check input
-
- $( ".sudoku" ).on( "keyup", function(e) {
-        var userInput = e.target.value;
-console.log(userInput);
-        
-      } )
-
-    
-
-//function verif Line :
-function verifline(){
-  
 }
 
-//function verif column :
-function verifcolumn(){}
+function checkMistakes(clickedInput) {
+    var rowValues = [];
+    var columnValues = [];
 
-//function verif 3*3 box :
-function verifmat(){}
-///////function to get my cas id which is selected by user onclick
+    var rowIndex = $(clickedInput).index() % 9;
+    var colIndex = Math.floor($(clickedInput).index() / 9);
 
+    $(".cube").each(function (index) {
+        var value = $(this).val();
+        var currentRowIndex = index % 9;
+        var currentColIndex = Math.floor(index / 9);
 
+        if (value && value !== "" && currentRowIndex === rowIndex) {
+            rowValues.push(value);
+        }
+
+        if (value && value !== "" && currentColIndex === colIndex) {
+            columnValues.push(value);
+        }
+    });
+
+    var mistakeDetected = hasDuplicates(rowValues) || hasDuplicates(columnValues);
+
+    if (mistakeDetected) {
+        console.log("Mistake detected!");
+        mistakes++;
+    } else {
+        console.log("No mistakes.");
+    }
+
+    return mistakeDetected;
+}
+
+function hasDuplicates(array) {
+    var seen = {};
+    for (var i = 0; i < array.length; i++) {
+        if (seen[array[i]]) {
+            return true;
+        }
+        seen[array[i]] = true;
+    }
+    return false;
+}
+
+function updateScore() {
+    score++;
+    $("#score").text("SCORE " + score);
+}
+
+function updateMistakes() {
+    $("#mistakes").text("MISTAKES " + mistakes);
+}
+
+function resetSudoku() {
+    $(".cube").val("");
+    score = 0;
+    mistakes = 0;
+    updateScore();
+    updateMistakes();
+}
+
+$(".btn").on("click", function () {
+    resetSudoku();
+});
+
+makeSudoku();
+clicked();
