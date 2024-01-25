94f163cbecc12aea5dbba4b16ad2497f001f12fa
Gatri Mahdi <gatrimahdi5@gmail.com>
2024-01-25 14:50:51 +0100

    first

--- sudoku.js
+++ sudoku.js
@@ -4,55 +4,52 @@
 //var Matrix=[[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null]]
 // input variable :
 var x =$("sudoku")
-
+clicked()
 
 
 //function makeSudoku cases :
-var arr = []
-var arr2 =[] 
-function makeSudoku (){
-
-    for (var i=1;i<=9;i++){
-        for (var j=1 ; j<=9;j++){
-            var cas=document.createElement("input")
-            cas.id= i
-            cas.id2= j 
-            var x =cas.id
-        var z = cas.id2
-            cas.classList.add("cube")
-            $(".sudoku").append(cas)
-        arr.push(x)
-        arr2.push(z)
+var arr = [];
+ 
+    function makeSudoku() {
+        for (var i = 1; i <= 9; i++) {
+            for (var j = 1; j <= 9; j++) {
+                var cas = document.createElement("input");
+                cas.id =   i + " " + j;
+                cas.classList.add("cube");
+                $(".sudoku").append(cas);
+                arr.push(cas.id);
+            }
         }
-        
     }
-}
 
 
-
-
- function clicked (){
-    var h = arr2.splice(0,1)
-   
-  //t= $("input").val(arr2[0])
-  for(var i = 0 ; i<h.length;i++){
- console.log(t= $("input").val(arr2[0]));
- 
-   
+function clicked() {
+    $(".cube").on("click", function () {
+        var clickedId = this.id;
+        console.log("Clicked ID: " + clickedId);
+    });
     
-  }
- 
-  
-   }
+}
    
 
 
 
             
 makeSudoku()
+// check input
+
+ $( ".sudoku" ).on( "keyup", function(e) {
+        var userInput = e.target.value;
+console.log(userInput);
+        
+      } )
+
+    
 
 //function verif Line :
-function verifline(){}
+function verifline(){
+  
+}
 
 //function verif column :
 function verifcolumn(){}
@@ -61,4 +58,4 @@ function verifcolumn(){}
 function verifmat(){}
 ///////function to get my cas id which is selected by user onclick
 
-clicked()
+
